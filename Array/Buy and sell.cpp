cc5976a22a94355140f67cc3fd59811a27f10a92
DIVYALAKSHMI P <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-09-11 07:18:27 +0530

    Create Buy and sell.cpp

---
+++ Array/Buy and sell.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+void buyandsell(int arr[] , int n)
+{
+    int profit , minprice = INT_MAX , maxprofit = INT_MIN;
+
+    for(int i=0; i<n; i++)
+    {
+        minprice = min(minprice , arr[i]);
+
+        maxprofit = max(maxprofit , (arr[i]-minprice));
+    }
+
+    cout<< maxprofit << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i=0;i<n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    buyandsell(arr , n);
+}
